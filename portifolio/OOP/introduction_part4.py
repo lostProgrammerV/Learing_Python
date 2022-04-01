26199202b4bc3eb41f6a682f825e60b13d3b3b2a
Programmer Vini <100801106+lostProgrammerV@users.noreply.github.com>
2022-04-01 15:48:36 -0300

    sumary

--- portifolio/OOP/introduction_part4.py
+++ portifolio/OOP/introduction_part4.py
@@ -1,12 +1,26 @@
 class Person:
     number_of_people = 0
+    gravity = -9.8
 
     def __init__(self,name):
         self.name = name
+        Person.add_person()
+
+    @classmethod    
+    def number_of_people(cls):
+        return cls.number_of_people
+
+    @classmethod
+    def add_person(cls):
+        cls.number_of_people += 1    
 
 p1 = Person("Aler")
 p2 = Person("Jill")
 
-Person.number_of_people = 8
+#Person.number_of_people = 8
 
-print(p2.number_of_people)
+p1 = Person("Tim")
+print(Person.number_of_people)
+P2 = Person("Jill")
+print(Person.number_of_people)
+print(Person.number_of_people())
