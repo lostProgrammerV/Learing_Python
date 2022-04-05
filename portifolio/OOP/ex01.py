017180985d7d5fc813243aa7af216425dac52dd2
Programmer Vini <100801106+lostProgrammerV@users.noreply.github.com>
2022-04-05 13:33:38 -0300

    Update ex01.py

--- portifolio/OOP/ex01.py
+++ portifolio/OOP/ex01.py
@@ -1,8 +1,13 @@
 class Quiz:
-    @staticmethod
-    def quiz_anwsers(anwser,correct):
+    @classmethod
+    def quiz_anwsers(anwser):
         anwser = str(input("What does CPU stand for ?"))
+        if anwser == "Central processing unit":
+            print("Correct")
+        else:
+            print("Incorrect")
 
+    @classmethod
     def playing(anwser):
         anwser = str(input("Do you want a play ?"))
         if anwser != "yes":
