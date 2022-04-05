e6b1fb59ea4fd06f76a1c738ddfe7a42e22d4da4
Programmer Vini <100801106+lostProgrammerV@users.noreply.github.com>
2022-04-05 12:57:28 -0300

    Update ex01.py

--- portifolio/OOP/ex01.py
+++ portifolio/OOP/ex01.py
@@ -1,3 +1,19 @@
 class Quiz:
-    def playing(self,anwser):
-        
+    @staticmethod
+    def quiz_anwsers(anwser,correct):
+        anwser = str(input("What does CPU stand for ?"))
+
+    def playing(anwser):
+        anwser = str(input("Do you want a play ?"))
+        if anwser != "yes":
+            quit()
+        print("Let's play :)") 
+  
+    
+
+
+
+
+
+
+
