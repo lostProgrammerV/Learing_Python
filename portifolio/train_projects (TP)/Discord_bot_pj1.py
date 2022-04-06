52f25b713b4a323edb562ecf40df5e4f22b020ae
Programmer Vini <100801106+lostProgrammerV@users.noreply.github.com>
2022-04-06 16:26:16 -0300

    Update_bot_description

---
+++ portifolio/train_projects (TP)/Discord_bot_pj1.py
@@ -0,0 +1,24 @@
+#This code was made in the replit
+#I added it in vscode to be able to display it in github
+#PS: That's my frist bot, sorry for being so simple
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
