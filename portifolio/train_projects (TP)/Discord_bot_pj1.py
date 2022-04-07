dad8fac59fb419152457acde97bec19258de4f8a
Programmer Vini <100801106+lostProgrammerV@users.noreply.github.com>
2022-04-07 13:47:56 -0300

    Update Discord_bot_pj1.py

--- portifolio/train_projects (TP)/Discord_bot_pj1.py
+++ portifolio/train_projects (TP)/Discord_bot_pj1.py
@@ -4,6 +4,11 @@ from discord.ext import commads
 
 bot = comands.bot('#')
 
+@bot.event
+async def on_ready():
+    print(f"I'm ready!")
+
+
 bot.run("OTYxMzQ4NDEzMjg1MzU1NjEw.Yk3reg.GH74i7Qb2ZNr02vqHOghLUMyPGM")
 
 
