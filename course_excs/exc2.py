0e5495949aa18ad070ad7998e8c272e69635d5e1
Programmer Vini <100801106+lostProgrammerV@users.noreply.github.com>
2022-04-07 16:03:57 -0300

    Update_all

---
+++ course_excs/exc2.py
@@ -0,0 +1,16 @@
+#this exercise has been changed to be more complex
+#more fun
+
+name = str(input('Input your name: '))
+
+print('Welcome! ',name,'good afternoon/morning/night\n'
+'do you want something?')
+
+anwser = str(input())
+
+if 'y' or 'yes' or 1:
+    print('Ok! what do u want ?')
+    if 'nothing' or 'nt':
+        print('Thx for our talk')
+else:
+    print('OK see around')
---
+++ course_excs/exc3.py
@@ -0,0 +1,12 @@
+#this exc is about adiction some values and has been
+#chagend and improving and some fun, i think
+
+value_1 = int(input('Input some value: '))
+value_2 = int(input('Input another some value: '))
+
+if value_1 <  value_2:
+    adction = value_1 + value_2
+    print('The result is: ', adction)
+else:
+    adction = value_2 + value_1
+    print('The result is: ', adction)
