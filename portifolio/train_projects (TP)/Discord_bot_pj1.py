4c9154ca23900b27cd84e8153e3cd85be49fd938
Programmer Vini <100801106+lostProgrammerV@users.noreply.github.com>
2022-04-07 13:51:19 -0300

    Update Discord_bot_pj1.py

--- portifolio/train_projects (TP)/Discord_bot_pj1.py
+++ portifolio/train_projects (TP)/Discord_bot_pj1.py
@@ -8,7 +8,11 @@ bot = comands.bot('#')
 async def on_ready():
     print(f"I'm ready!")
 
+async def send_hi(msg):
+    name = msg.author.name
+    anwser = "hi, " + name
 
+    await msg.send(anwser)
 bot.run("OTYxMzQ4NDEzMjg1MzU1NjEw.Yk3reg.GH74i7Qb2ZNr02vqHOghLUMyPGM")
 
 
