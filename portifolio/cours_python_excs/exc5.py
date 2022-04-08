1ed32d8ed218141b510a06a7e0100cf02a4c34f9
Programmer Vini <100801106+lostProgrammerV@users.noreply.github.com>
2022-04-08 12:27:28 -0300

    Update exc5.py

--- portifolio/cours_python_excs/exc5.py
+++ portifolio/cours_python_excs/exc5.py
@@ -3,4 +3,4 @@ numb = int(input('Answer some number: '))
 prql = numb - 1
 fwrd = numb + 1
 
-print('prequel value is ', prql,' and foward value is ', fwrd)
+print(f'prequel value is {prql} and foward value is {fwrd}')
