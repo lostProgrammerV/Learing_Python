3596838811bff7942c75e2b7a4aa6c011cb98274
Programmer Vini <100801106+lostProgrammerV@users.noreply.github.com>
2022-04-08 14:16:36 -0300

    update

---
+++ portifolio/cours_python_excs/exc6.py
@@ -0,0 +1,7 @@
+value = int(input('Input some value: '))
+
+double = value * 2
+triple = value * 3
+root = value ** (1 / 2)
+
+print(f'the double value is {double}\n, triple of the value is {triple}\n and the root of the value is {root:.2f}')
