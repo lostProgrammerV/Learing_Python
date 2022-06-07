d01907f8ed7c1a938636ddc0168635e0d0df9de3
Programmer Vini <100801106+lostProgrammerV@users.noreply.github.com>
2022-06-07 16:23:14 -0300

    Update tic_tac_toe_w_ai.py

--- portifolio/train_projects (TP)/tic-tac-toes/tic_tac_toe_w_ai.py
+++ portifolio/train_projects (TP)/tic-tac-toes/tic_tac_toe_w_ai.py
@@ -1,5 +1,12 @@
+board = [' ' for x in range(10)]
+
+def print_board(board):
+    print(board[1] + board[2] + board[3] + '\n'
+        + board[4] + board[5] + board[6] + '\n'
+        + board[7] + board[8] + board[9])
+
 def insertBoard(letter, pos):
-    board = [' ' for x in range(10)]
+    pass
 
 def insertLetter(letter, pos):
     board[pos] = letter    
@@ -7,8 +14,15 @@ def insertLetter(letter, pos):
 def spaceIsFree(pos):
     return board[pos] == ''
 
-def isWinner():
-    pass
+def isWinner(bo, le):
+    return ((bo[7] == le and bo[8] == le and bo[9] == le) or
+    (bo[4] == le and bo[5] == le and bo[6] == le) or 
+    (bo[1] == le and bo[2] == le and bo[3] == le) or 
+    (bo[7] == le and bo[4] == le and bo[1] == le) or 
+    (bo[8] == le and bo[5] == le and bo[2] == le) or 
+    (bo[9] == le and bo[6] == le and bo[3] == le) or 
+    (bo[7] == le and bo[5] == le and bo[3] == le) or 
+    (bo[9] == le and bo[5] == le and bo[1] == le))
 
 def playerMove():
     pass
@@ -20,9 +34,6 @@ def compMove():
     pass
 
 def isBoardFull(board):
-    pass
-
-def printBoard():
     pass
 
 def main():
