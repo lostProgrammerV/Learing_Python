39477e600f3f80bc35d069bb48b204cf02eb1a74
Programmer Vini <100801106+lostProgrammerV@users.noreply.github.com>
2022-06-07 15:55:20 -0300

    Updates

---
+++ portifolio/train_projects (TP)/tic-tac-toes/tic_tac_toe_w_ai.py
@@ -0,0 +1,36 @@
+def insertBoard(letter, pos):
+    board = [' ' for x in range(10)]
+
+def insertLetter(letter, pos):
+    board[pos] = letter    
+
+def spaceIsFree(pos):
+    return board[pos] == ''
+
+def isWinner():
+    pass
+
+def playerMove():
+    pass
+
+def selectRandom():
+    pass
+
+def compMove():
+    pass
+
+def isBoardFull(board):
+    pass
+
+def printBoard():
+    pass
+
+def main():
+    pass
+
+
+
+
+
+
+
